6690dbc81891f78ee46beb6278c6b4a238464dfb
Sam Clegg <sam@superduper.net>
2011-06-10 15:52:52 -0700

    initial marmlade patch

--- upstream/include/ode/odeconfig.h
+++ upstream/include/ode/odeconfig.h
@@ -43,19 +43,19 @@
 /* Well-defined common data types...need to define for 64 bit systems */
 #if defined(_M_IA64) || defined(__ia64__) || defined(_M_AMD64) || defined(__x86_64__)
   #define X86_64_SYSTEM   1
-  typedef int             int32;
-  typedef unsigned int    uint32;
-  typedef short           int16;
-  typedef unsigned short  uint16;
-  typedef char            int8;
-  typedef unsigned char   uint8;
+  //typedef int             int32;
+  //typedef unsigned int    uint32;
+  //typedef short           int16;
+  //typedef unsigned short  uint16;
+  //typedef char            int8;
+  //typedef unsigned char   uint8;
 #else
-  typedef int             int32;
-  typedef unsigned int    uint32;
-  typedef short           int16;
-  typedef unsigned short  uint16;
-  typedef char            int8;
-  typedef unsigned char   uint8;
+  //typedef int             int32;
+  //typedef unsigned int    uint32;
+  //typedef short           int16;
+  //typedef unsigned short  uint16;
+  //typedef char            int8;
+  //typedef unsigned char   uint8;
 #endif
 
 /* Visual C does not define these functions */
